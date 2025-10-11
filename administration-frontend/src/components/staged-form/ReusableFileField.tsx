bfca823038620a3d214fc10a61a401a1200e0648
Lukáš Kaňka <lukaskabc@gmail.com>
2025-10-11 13:59:55 +0200

    Add reusable file list for displaying selected files

--- administration-frontend/src/components/staged-form/ReusableFileField.tsx
+++ administration-frontend/src/components/staged-form/ReusableFileField.tsx
@@ -1,7 +1,8 @@
 import { type FieldProps, type FormContextType, type RJSFSchema, type StrictRJSFSchema } from '@rjsf/utils'
 import { ReusableFile } from '@/model/ReusableFile.ts'
-import { useCallback } from 'preact/hooks'
+import { useCallback, useState } from 'preact/hooks'
 import ReusableFileWidget from '@/components/staged-form/ReusableFileWidget.tsx'
+import ReusableFileList from '@/components/staged-form/ReusableFileList.tsx'
 
 /**
  *  File field holding an array of {@link ReusableFile ReusableFiles}
@@ -13,32 +14,42 @@ function ReusableFileField<
 >(props: FieldProps<T, S, F>) {
   const { onChange, fieldPathId } = props
   props.multiple = !!(props.multiple || props.options?.multiple)
+  const [value, setValue] = useState(props.formData)
 
-  const onInputChange = useCallback((files: ReusableFile[]) => onChange(files as T, fieldPathId.path), [onChange])
+  const onInputChange = useCallback(
+    (files: ReusableFile[]) => {
+      setValue(files as T)
+      onChange(files as T, fieldPathId.path)
+    },
+    [onChange]
+  )
 
   return (
-    <ReusableFileWidget
-      options={{ ...props.uiSchema }}
-      schema={props.schema}
-      uiSchema={props.uiSchema}
-      id={fieldPathId.$id}
-      name={props.name}
-      label={props.label}
-      hideLabel={!props.displayLabel}
-      hideError={props.hideError}
-      value={props.formData}
-      onChange={onInputChange}
-      onBlur={props.onBlur}
-      onFocus={props.onFocus}
-      required={props.required}
-      disabled={props.disabled}
-      readonly={props.readonly}
-      formContext={props.formContext}
-      autofocus={props.autofocus}
-      registry={props.registry}
-      placeholder={props.placeholder}
-      rawErrors={props.rawErrors}
-    />
+    <>
+      <ReusableFileWidget
+        options={{ ...props.uiSchema }}
+        schema={props.schema}
+        uiSchema={props.uiSchema}
+        id={fieldPathId.$id}
+        name={props.name}
+        label={props.label}
+        hideLabel={!props.displayLabel}
+        hideError={props.hideError}
+        value={value}
+        onChange={onInputChange}
+        onBlur={props.onBlur}
+        onFocus={props.onFocus}
+        required={props.required}
+        disabled={props.disabled}
+        readonly={props.readonly}
+        formContext={props.formContext}
+        autofocus={props.autofocus}
+        registry={props.registry}
+        placeholder={props.placeholder}
+        rawErrors={props.rawErrors}
+      />
+      <ReusableFileList files={value} />
+    </>
   )
 }
 
---
+++ administration-frontend/src/components/staged-form/ReusableFileList.tsx
@@ -0,0 +1,69 @@
+import type { ReusableFile } from '@/model/ReusableFile.ts'
+import { useMemo } from 'preact/hooks'
+import type { TreeViewBaseItem } from '@mui/x-tree-view/models'
+import { RichTreeView } from '@mui/x-tree-view'
+
+export type ReusableFileListProps = {
+  files?: ReusableFile[]
+}
+
+function findTreeRoot(label: string, tree: TreeViewBaseItem[]): TreeViewBaseItem | null {
+  for (let rootNode of tree) {
+    if (rootNode.label === label) {
+      return rootNode
+    }
+  }
+
+  return null
+}
+
+function findTreeNodeChild(label: string, node: TreeViewBaseItem): TreeViewBaseItem | null {
+  if (node && node.children) {
+    for (let child of node.children) {
+      if (child.label === label) {
+        return child
+      }
+    }
+  }
+  return null
+}
+
+function insertToTree(file: ReusableFile, tree: TreeViewBaseItem[]) {
+  const path = file.fileName?.split('/')
+  if (!path || path.length === 0) {
+    console.error('Unable to process file', file)
+    return
+  }
+
+  let currentPath = path[0]
+  let current: TreeViewBaseItem | null = findTreeRoot(path[0], tree)
+  if (!current) {
+    current = { id: path[0], label: path[0], children: [] }
+    tree.push(current)
+  }
+  for (let i = 1; i < path.length; i++) {
+    const label = path[i]
+    currentPath += '/' + label
+
+    let found = findTreeNodeChild(label, current)
+    if (!found) {
+      found = { id: currentPath, label, children: [] }
+      current.children?.push(found)
+    }
+    current = found
+  }
+}
+
+export default function ReusableFileList({ files }: ReusableFileListProps) {
+  const treeData: TreeViewBaseItem[] = useMemo(() => {
+    const result: TreeViewBaseItem[] = []
+    if (files) {
+      for (let f of files) {
+        insertToTree(f, result)
+      }
+    }
+    return result
+  }, [files])
+
+  return <RichTreeView items={treeData} />
+}

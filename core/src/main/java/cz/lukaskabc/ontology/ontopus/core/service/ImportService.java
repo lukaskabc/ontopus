ab88b99baee25e88d04f453f300eabe4f7295434
Lukáš Kaňka <lukaskabc@gmail.com>
2026-02-20 11:37:11 +0100

    extract file resolving from import service

--- core/src/main/java/cz/lukaskabc/ontology/ontopus/core/service/ImportService.java
+++ core/src/main/java/cz/lukaskabc/ontology/ontopus/core/service/ImportService.java
@@ -4,98 +4,39 @@ import cz.lukaskabc.ontology.ontopus.api.model.JsonForm;
 import cz.lukaskabc.ontology.ontopus.core.rest.request.FormFileRequest;
 import cz.lukaskabc.ontology.ontopus.core.rest.request.ImportProcessContextRequest;
 import cz.lukaskabc.ontology.ontopus.core.service.process.ImportProcessMediator;
-import cz.lukaskabc.ontology.ontopus.core.util.ConsumableInputStreamSource;
-import cz.lukaskabc.ontology.ontopus.core_model.exception.OntopusException;
-import cz.lukaskabc.ontology.ontopus.core_model.exception.ValidationException;
+import cz.lukaskabc.ontology.ontopus.core.util.RequestFileResolver;
 import cz.lukaskabc.ontology.ontopus.core_model.model.id.VersionSeriesURI;
+import org.jspecify.annotations.NullMarked;
 import org.jspecify.annotations.Nullable;
 import org.springframework.core.io.InputStreamSource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.MultiValueMap;
-import org.springframework.validation.Validator;
 import org.springframework.web.multipart.MultipartFile;
 import tools.jackson.databind.JsonNode;
 import tools.jackson.databind.ObjectMapper;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.concurrent.Future;
 
+@NullMarked
 @Service
 public class ImportService {
-    private static final String UPLOAD_TEMP_FILE_PREFIX = "ontopus-uploaded-file_";
-
-    private static MultipartFile findUploadedFile(
-            MultiValueMap<String, MultipartFile> files, String formFieldName, String fileName) {
-        List<MultipartFile> fieldFiles = files.get(formFieldName);
-        if (fieldFiles != null) {
-            for (MultipartFile file : fieldFiles) {
-                if (fileName.equals(file.getOriginalFilename())) {
-                    return file;
-                }
-            }
-        }
-        throw new IllegalStateException("Could not find uploaded file for " + fileName);
-    }
-
-    /**
-     * {@link MultipartFile MultipartFile} to transfer to a system temporary file.
-     *
-     * @param uploadedFile the file to transfer
-     * @return {@link InputStreamSource} providing the input stream with file contents. The temporary file will be
-     *     automatically deleted when this object is garbage collected.
-     * @see ConsumableInputStreamSource
-     */
-    private static InputStreamSource transferUploadedFile(MultipartFile uploadedFile) {
-        try {
-            File tempFile = Files.createTempFile(UPLOAD_TEMP_FILE_PREFIX, null).toFile();
-            uploadedFile.transferTo(tempFile);
-            return new ConsumableInputStreamSource(tempFile);
-        } catch (IOException e) {
-            throw new OntopusException(e); // TODO exception
-        }
-    }
 
     private final ImportProcessMediator mediator;
 
     private final ObjectMapper objectMapper;
+    private final RequestFileResolver requestFileResolver;
 
-    private final Validator validator; // TODO use FileUtils to check paths of uploaded files and prevent path
-    // traversal
-
-    public ImportService(ImportProcessMediator mediator, ObjectMapper objectMapper, Validator validator) {
+    public ImportService(
+            ImportProcessMediator mediator, ObjectMapper objectMapper, RequestFileResolver requestFileResolver) {
         this.mediator = mediator;
         this.objectMapper = objectMapper;
-        this.validator = validator;
+        this.requestFileResolver = requestFileResolver;
     }
 
-    /**
-     * Finds all reusable files inside {@code jsonData}
-     *
-     * @param jsonData the data to search
-     * @return the list
-     */
-    private List<FormFileRequest> findReusableFiles(Iterator<JsonNode> jsonData) {
-        ArrayList<FormFileRequest> reusableFiles = new ArrayList<>();
-        findReusableFiles(jsonData, reusableFiles);
-        reusableFiles.trimToSize();
-        return reusableFiles;
-    }
-
-    private void findReusableFiles(Iterator<JsonNode> iterator, List<FormFileRequest> reusableFiles) {
-        while (iterator.hasNext()) {
-            final JsonNode node = iterator.next();
-            if (FormFileRequest.matches(node)) {
-                reusableFiles.add(objectMapper.convertValue(node, FormFileRequest.class));
-            } else {
-                findReusableFiles(node.iterator(), reusableFiles);
-            }
-        }
-    }
-
-    public Future<JsonForm> getCurrentJsonForm() {
+    public Future<@Nullable JsonForm> getCurrentJsonForm() {
         return mediator.getCurrentForm(); // TODO include form data when publishing new version of
     }
 
@@ -106,38 +47,15 @@ public class ImportService {
     private Map<FormFileRequest, InputStreamSource> resolveCombinedFiles(
             ImportProcessContextRequest contextRequest, MultiValueMap<String, MultipartFile> files) {
         Map<FormFileRequest, InputStreamSource> result = new HashMap<>(files.size());
-        contextRequest.getServiceToReusableFormResultMap().forEach((serviceId, formResult) -> {
+        contextRequest.getServiceToFormResultMap().values().forEach((formResult) -> {
             Iterator<JsonNode> jsonDataIterator =
                     formResult.values().stream().map(objectMapper::readTree).iterator();
-            result.putAll(resolveFiles(jsonDataIterator, files));
+            result.putAll(requestFileResolver.resolveFiles(jsonDataIterator, files));
         });
         return result;
     }
 
-    /**
-     * Finds reusable files in the jsonData
-     *
-     * @param files
-     */
-    private Map<FormFileRequest, InputStreamSource> resolveFiles(
-            Iterator<JsonNode> jsonData, MultiValueMap<String, MultipartFile> files) {
-        final List<FormFileRequest> reusableFiles = findReusableFiles(jsonData); // TODO rename from reusable files
-        validator.validateObject(reusableFiles).failOnError(ValidationException::new);
-        final Map<FormFileRequest, InputStreamSource> result = new HashMap<>(reusableFiles.size());
-
-        for (final FormFileRequest fileRequest : reusableFiles) {
-            MultipartFile multipartFile =
-                    findUploadedFile(files, fileRequest.getFormFieldName(), fileRequest.getFileName());
-
-            InputStreamSource streamSource = transferUploadedFile(multipartFile);
-
-            Objects.requireNonNull(streamSource);
-            result.put(fileRequest, streamSource);
-        }
-        return result;
-    }
-
-    public Future<?> submitCombinedData(
+    public Future<@Nullable Void> submitCombinedData(
             ImportProcessContextRequest context, MultiValueMap<String, MultipartFile> files) {
         Map<FormFileRequest, InputStreamSource> reusableFiles = resolveCombinedFiles(context, files);
         // return mediator.submitCombinedFormResult(context, reusableFiles);
@@ -145,9 +63,9 @@ public class ImportService {
         throw new UnsupportedOperationException("Not yet implemented");
     }
 
-    public Future<?> submitData(Map<String, JsonNode> jsonData, MultiValueMap<String, MultipartFile> files) {
+    public Future<Void> submitData(Map<String, JsonNode> jsonData, MultiValueMap<String, MultipartFile> files) {
         Map<FormFileRequest, InputStreamSource> reusableFiles =
-                resolveFiles(jsonData.values().iterator(), files);
+                requestFileResolver.resolveFiles(jsonData.values().iterator(), files);
         return mediator.submitFormResult(jsonData, reusableFiles);
     }
 }
---
+++ core/src/main/java/cz/lukaskabc/ontology/ontopus/core/util/RequestFileResolver.java
@@ -0,0 +1,132 @@
+package cz.lukaskabc.ontology.ontopus.core.util;
+
+import cz.lukaskabc.ontology.ontopus.core.exception.FileOperationException;
+import cz.lukaskabc.ontology.ontopus.core.rest.request.FormFileRequest;
+import cz.lukaskabc.ontology.ontopus.core_model.exception.ValidationException;
+import org.jspecify.annotations.NullMarked;
+import org.springframework.core.io.InputStreamSource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
+import org.springframework.validation.Validator;
+import org.springframework.web.multipart.MultipartFile;
+import tools.jackson.databind.JsonNode;
+import tools.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.*;
+
+/** Utility component capable of resolving uploaded files for {@link FormFileRequest} */
+@NullMarked
+@Component
+public class RequestFileResolver {
+    /** The prefix of temporary files created for uploaded files. */
+    static final String UPLOAD_TEMP_FILE_PREFIX = "ontopus-uploaded-file_";
+    /**
+     * Transfers the provided uploaded file to a temporary file and returns an {@link InputStreamSource} for it. The
+     * temporary file will be automatically deleted when the returned {@link InputStreamSource} is garbage collected.
+     *
+     * @param uploadedFile the uploaded file to transfer
+     * @return {@link InputStreamSource} providing the input stream with file contents.
+     * @see ConsumableInputStreamSource
+     */
+    public static InputStreamSource transferToTempFile(MultipartFile uploadedFile) {
+        Objects.requireNonNull(uploadedFile, "Uploaded file must not be null");
+        try {
+            File tempFile = Files.createTempFile(UPLOAD_TEMP_FILE_PREFIX, null).toFile();
+            uploadedFile.transferTo(tempFile);
+            return new ConsumableInputStreamSource(tempFile);
+        } catch (IOException e) {
+            throw new FileOperationException("Failed to transfer multipart file to a new temporary file", e);
+        }
+    }
+
+    private final Validator validator;
+
+    private final ObjectMapper objectMapper;
+
+    public RequestFileResolver(Validator validator, ObjectMapper objectMapper) {
+        this.validator = validator;
+        this.objectMapper = objectMapper;
+    }
+
+    /**
+     * Finds all {@link FormFileRequest} instances in the provided JSON data, including nested objects.
+     *
+     * @param jsonNodeIterator Iterator over JSON nodes to search for {@link FormFileRequest} instances.
+     * @return List of found {@link FormFileRequest} instances.
+     */
+    private List<FormFileRequest> findFileRequests(Iterator<JsonNode> jsonNodeIterator) {
+        ArrayList<FormFileRequest> reusableFiles = new ArrayList<>();
+        findFileRequests(jsonNodeIterator, reusableFiles);
+        reusableFiles.trimToSize();
+        return Collections.unmodifiableList(reusableFiles);
+    }
+
+    /**
+     * Recursively searches for {@link FormFileRequest} instances in the provided JSON nodes and adds them to the list.
+     *
+     * @param iterator Iterator over JSON nodes to search for {@link FormFileRequest} instances.
+     * @param fileRequests List to collect found {@link FormFileRequest} instances.
+     */
+    private void findFileRequests(Iterator<JsonNode> iterator, List<FormFileRequest> fileRequests) {
+        while (iterator.hasNext()) {
+            final JsonNode node = iterator.next();
+            if (FormFileRequest.matches(node)) {
+                fileRequests.add(objectMapper.convertValue(node, FormFileRequest.class));
+            } else {
+                findFileRequests(node.iterator(), fileRequests);
+            }
+        }
+    }
+
+    /**
+     * Finds the uploaded file corresponding to the provided {@link FormFileRequest} in the uploaded files map.
+     *
+     * @param files Map of uploaded files, the key is the form field name, the value is a list of uploaded files for
+     *     that field.
+     * @param formFileRequest The {@link FormFileRequest} to search for
+     * @return The {@link MultipartFile} corresponding to the provided {@link FormFileRequest}.
+     * @throws IllegalStateException if no matching file is found in the uploaded files map.
+     */
+    private MultipartFile findUploadedFile(
+            MultiValueMap<String, MultipartFile> files, FormFileRequest formFileRequest) {
+        final String formFieldName = formFileRequest.getFormFieldName();
+        final String fileName = formFileRequest.getFileName();
+        List<MultipartFile> fieldFiles = files.get(formFieldName);
+        if (fieldFiles != null) {
+            for (MultipartFile file : fieldFiles) {
+                if (fileName.equals(file.getOriginalFilename())) {
+                    return file;
+                }
+            }
+        }
+        throw new IllegalStateException("Could not find uploaded file for " + fileName);
+    }
+
+    /**
+     * Resolves files from the request data. Resolved files are copied to temporary files and provided as
+     * {@link InputStreamSource} to allow their asynchronous processing. The temporary files will be automatically
+     * deleted when the corresponding {@link InputStreamSource} instances are garbage collected.
+     *
+     * @param jsonData JSON objects that may contain {@link FormFileRequest}
+     * @param files Uploaded files from the request, mapped by form field name.
+     * @return Mapping of {@link FormFileRequest} to {@link InputStreamSource} for the corresponding uploaded files.
+     */
+    public Map<FormFileRequest, InputStreamSource> resolveFiles(
+            Iterator<JsonNode> jsonData, MultiValueMap<String, MultipartFile> files) {
+        final List<FormFileRequest> reusableFiles = findFileRequests(jsonData);
+        validator.validateObject(reusableFiles).failOnError(ValidationException::new);
+        final Map<FormFileRequest, InputStreamSource> result = new HashMap<>(reusableFiles.size());
+
+        for (final FormFileRequest fileRequest : reusableFiles) {
+            MultipartFile multipartFile = findUploadedFile(files, fileRequest);
+            InputStreamSource streamSource = transferToTempFile(multipartFile);
+
+            Objects.requireNonNull(streamSource);
+            result.put(fileRequest, streamSource);
+        }
+        return result;
+    }
+}

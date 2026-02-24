7bcbd68252d3290ca700ded30574089a21eb7d3a
Lukáš Kaňka <lukaskabc@gmail.com>
2026-02-24 17:44:05 +0100

    fix jopa enum usage

--- core-model/src/main/java/cz/lukaskabc/ontology/ontopus/core_model/model/request_mapping/MappingType.java
+++ core-model/src/main/java/cz/lukaskabc/ontology/ontopus/core_model/model/request_mapping/MappingType.java
@@ -3,12 +3,25 @@ package cz.lukaskabc.ontology.ontopus.core_model.model.request_mapping;
 import cz.cvut.kbss.jopa.model.annotations.Individual;
 import cz.lukaskabc.ontology.ontopus.core_model.generated.Vocabulary;
 
+import java.net.URI;
+
 /** The type of a resource mapping */
 public enum MappingType {
     /** The mapping is for the main ontology document */
     @Individual(iri = Vocabulary.s_i_OntologyDocument)
-    ONTOLOGY_DOCUMENT,
+    ONTOLOGY_DOCUMENT(Vocabulary.u_i_OntologyDocument),
     /** The mapping is for all resources defined in the ontology document */
     @Individual(iri = Vocabulary.s_i_OntologyResource)
-    RESOURCE
+    RESOURCE(Vocabulary.u_i_OntologyResource),
+    ;
+
+    private final URI uri;
+
+    MappingType(URI uri) {
+        this.uri = uri;
+    }
+
+    public URI getUri() {
+        return uri;
+    }
 }
--- core-model/src/main/java/cz/lukaskabc/ontology/ontopus/core_model/persistence/dao/ContextToControllerMappingDao.java
+++ core-model/src/main/java/cz/lukaskabc/ontology/ontopus/core_model/persistence/dao/ContextToControllerMappingDao.java
@@ -1,7 +1,6 @@
 package cz.lukaskabc.ontology.ontopus.core_model.persistence.dao;
 
 import cz.cvut.kbss.jopa.model.EntityManager;
-import cz.lukaskabc.ontology.ontopus.core_model.generated.Vocabulary;
 import cz.lukaskabc.ontology.ontopus.core_model.model.id.ContextToControllerMappingURI;
 import cz.lukaskabc.ontology.ontopus.core_model.model.id.GraphURI;
 import cz.lukaskabc.ontology.ontopus.core_model.model.request_mapping.ContextToControllerMapping;
@@ -11,8 +10,6 @@ import cz.lukaskabc.ontology.ontopus.core_model.persistence.DescriptorFactory;
 import cz.lukaskabc.ontology.ontopus.core_model.persistence.dao.base.AbstractDao;
 import org.jspecify.annotations.Nullable;
 import org.springframework.stereotype.Component;
-
-import java.net.URI;
 
 @Component
 public class ContextToControllerMappingDao
@@ -30,15 +27,7 @@ public class ContextToControllerMappingDao
                         "SELECT m FROM ContextToControllerMapping m WHERE m.mappingType = :mappingType AND m.subject = :graphURI",
                         entityClass)
                 .setMaxResults(1)
-                .setParameter("mappingType", mapType(mappingType))
+                .setParameter("mappingType", mappingType.getUri())
                 .setParameter("graphURI", graphURI.toURI())::getSingleResult);
-    }
-
-    private URI mapType(MappingType mappingType) {
-        return switch (mappingType) {
-            case ONTOLOGY_DOCUMENT -> Vocabulary.u_i_OntologyDocument;
-            case RESOURCE -> Vocabulary.u_i_OntologyResource;
-            default -> throw new IllegalArgumentException("Unsupported mapping type: " + mappingType);
-        };
     }
 }

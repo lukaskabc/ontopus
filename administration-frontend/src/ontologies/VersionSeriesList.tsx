76b8e6f4623652f83aec2289182d9322c5f8573a
Lukáš Kaňka <lukaskabc@gmail.com>
2026-03-06 16:19:36 +0100

    fix missing create button on ontologies list

--- administration-frontend/src/ontologies/VersionSeriesList.tsx
+++ administration-frontend/src/ontologies/VersionSeriesList.tsx
@@ -1,12 +1,17 @@
 import ResourceEntryList from '@/ontologies/ResourceEntryList.tsx'
 
 import { useTranslation } from 'react-i18next'
-import { useMemo } from 'preact/hooks'
+import { useCallback, useMemo } from 'preact/hooks'
 import { VersionSeriesDetail } from '@/ontologies/detail/series/VersionSeriesDetail.tsx'
 import { VersionSeriesListEntryDataSource } from '@/ontologies/VersionSeriesListEntryDataSource.ts'
+import { useLocation } from 'wouter-preact'
 
 export default function VersionSeriesList() {
+  const [_, navigate] = useLocation()
   const { i18n } = useTranslation()
   const dataSource = useMemo(() => new VersionSeriesListEntryDataSource(i18n), [i18n])
-  return <ResourceEntryList dataSource={dataSource} detailComponent={VersionSeriesDetail} />
+  const onCreateClick = useCallback(() => navigate(`/publish`), [navigate])
+  return (
+    <ResourceEntryList dataSource={dataSource} detailComponent={VersionSeriesDetail} onCreateClick={onCreateClick} />
+  )
 }

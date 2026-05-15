e767b6d9bd8e10b281fe4a6412968a3b4e02fc60
Lukáš Kaňka <lukaskabc@gmail.com>
2026-05-15 15:55:50 +0200

    Update javadoc for OntologyAnnotationInjectionService

--- plugin-api/src/main/java/cz/lukaskabc/ontology/ontopus/api/service/import_process/OntologyAnnotationInjectionService.java
+++ plugin-api/src/main/java/cz/lukaskabc/ontology/ontopus/api/service/import_process/OntologyAnnotationInjectionService.java
@@ -2,5 +2,9 @@ package cz.lukaskabc.ontology.ontopus.api.service.import_process;
 
 import org.eclipse.rdf4j.model.Model;
 
-/** Resolves statements to be injected into the original ontology */
+/**
+ * Resolves statements to be injected into the original ontology.
+ *
+ * @implNote the service will be put to the stack automatically and the returned model will be automatically persisted.
+ */
 public interface OntologyAnnotationInjectionService extends ImportProcessingService<Model> {}

d280a3f812aa1acb93c5555b67a233fb9c4fbdc5
Lukáš Kaňka <lukaskabc@gmail.com>
2026-08-05 13:44:12 +0200

    test: exclude test classes from architecture tests

--- tests/src/test/java/cz/lukaskabc/ontology/ontopus/tests/architecture/OntopusArchitectureTest.java
+++ tests/src/test/java/cz/lukaskabc/ontology/ontopus/tests/architecture/OntopusArchitectureTest.java
@@ -1,6 +1,7 @@
 package cz.lukaskabc.ontology.ontopus.tests.architecture;
 
 import com.tngtech.archunit.core.importer.ImportOption;
+import com.tngtech.archunit.core.importer.Location;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 
 import java.lang.annotation.ElementType;
@@ -12,8 +13,17 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @AnalyzeClasses(
         packages = {OntopusArchitectureTest.ONTOPUS_PACKAGES, OntopusArchitectureTest.JOPA_MODEL_PACKAGES},
-        importOptions = ImportOption.Predefined.DoNotIncludePackageInfos.class)
+        importOptions = OntopusArchitectureTest.ImportOptions.class)
 public @interface OntopusArchitectureTest {
     public static final String ONTOPUS_PACKAGES = "cz.lukaskabc.ontology.ontopus..";
     public static final String JOPA_MODEL_PACKAGES = "cz.cvut.kbss.jopa.model..";
+
+    public class ImportOptions implements ImportOption {
+
+        @Override
+        public boolean includes(Location location) {
+            return Predefined.DO_NOT_INCLUDE_TESTS.includes(location)
+                    && Predefined.DO_NOT_INCLUDE_PACKAGE_INFOS.includes(location);
+        }
+    }
 }

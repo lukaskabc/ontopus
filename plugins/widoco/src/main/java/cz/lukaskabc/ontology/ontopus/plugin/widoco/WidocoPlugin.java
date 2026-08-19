95d98d27af7998934850fdbb7a4cab6081acb964
Lukáš Kaňka <lukaskabc@gmail.com>
2026-08-19 13:50:05 +0200

    fix: Widoco plugin trying to access RDFPlugin class

--- plugins/widoco/src/main/java/cz/lukaskabc/ontology/ontopus/plugin/widoco/WidocoPlugin.java
+++ plugins/widoco/src/main/java/cz/lukaskabc/ontology/ontopus/plugin/widoco/WidocoPlugin.java
@@ -2,12 +2,11 @@ package cz.lukaskabc.ontology.ontopus.plugin.widoco;
 
 import cz.lukaskabc.ontology.ontopus.api.Plugin;
 import cz.lukaskabc.ontology.ontopus.core_model.exception.InitializationException;
-import cz.lukaskabc.ontology.ontopus.plugin.rdf.RDFPlugin;
 
 public class WidocoPlugin implements Plugin {
     public WidocoPlugin() {
         try {
-            Class.forName(RDFPlugin.class.getName());
+            Class.forName("cz.lukaskabc.ontology.ontopus.plugin.rdf.RDFPlugin");
         } catch (ClassNotFoundException e) {
             throw new InitializationException("WidocoPlugin requires RDFPlugin to be present on the classpath", e);
         }

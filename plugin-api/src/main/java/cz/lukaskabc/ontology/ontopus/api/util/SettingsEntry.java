bea5d6d0cf041ecc2d2ac6a72d1c3c0358e522c8
Lukáš Kaňka <lukaskabc@gmail.com>
2026-09-16 15:39:51 +0200

    feat #2: add settings entry for configuring URI aliases

--- plugin-api/src/main/java/cz/lukaskabc/ontology/ontopus/api/util/SettingsEntry.java
+++ plugin-api/src/main/java/cz/lukaskabc/ontology/ontopus/api/util/SettingsEntry.java
@@ -1,6 +1,7 @@
 package cz.lukaskabc.ontology.ontopus.api.util;
 
 import cz.lukaskabc.ontology.ontopus.api.model.FormJsonDataDto;
+import cz.lukaskabc.ontology.ontopus.api.model.JsonForm;
 import org.springframework.stereotype.Component;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.multipart.MultipartFile;
@@ -12,7 +13,31 @@ import org.springframework.web.multipart.MultipartFile;
  *     annotation)
  */
 @Component
-public interface SettingsEntry extends GlobalSettingsEntry {
+public interface SettingsEntry {
+    /**
+     * Provides JSON form that will be shown to the user
+     *
+     * @return the JSON form
+     */
+    JsonForm getForm();
+
+    /**
+     * A unique identifier of the entry
+     *
+     * @return the unique identifier
+     * @implSpec The identifier should be constant for the entry and should not change between application restarts. The
+     *     identifier must be unique.
+     */
+    default String getIdentifier() {
+        return this.getClass().getName();
+    }
+
+    /**
+     * Label of the menu entry.
+     *
+     * @return i18n key for the translation of the menu entry.
+     */
+    String getLabel();
 
     /** Handles the data submitted to the form. */
     void handleSubmit(FormJsonDataDto formData, MultiValueMap<String, MultipartFile> files);
